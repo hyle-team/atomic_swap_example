1ed79cb4b61e0c431b440b4c23fd8095689f21f9
cryptozoidberg <crypto.zoidberg@gmail.com>
2021-03-24 20:46:23 +0100

    Code clean up

--- app/run-swap-zano.js
+++ app/run-swap-zano.js
@@ -1,7 +1,12 @@
 /**
- * Run cross-chain atomic swap.
- * WARNING: Running this script will send transactions and spend coins!
+ * swap.js - cross-chain atomic swap manager for the Bitcoin and Zano
+ * Copyright (c) 2021, Zano developers (MIT License)
+ * https://github.com/hyle-team/zano 
+ * 
+ * Based on the code provided in https://github.com/pinheadmz/swap-test 
+ * and article https://bcoin.io/guides/swaps.html 
  */
+
 
  'use strict';
 
@@ -24,7 +29,6 @@ function sleep(ms)
   return new Promise(resolve => setTimeout(resolve, ms));
 }
 
-
 async function perform_swap()
 {
   const partyAlice = new SwapBundle();
@@ -35,13 +39,6 @@ async function perform_swap()
 
   await partyAlice.zano.init(12335); //Alice's zano wallet instance running on port 12335
   await partyBob.zano.init(12334); //Bob's's zano wallet instance running on port 12334
-  
-
-  //temporary code(remove it when it's working)
-  //partyAlice.btc.privateKey = new Buffer('eae90b84dff2413385ad18133f77230cbabe863aedf71394e91e115126661000', 'hex');
-  //partyBob.btc.privateKey = new Buffer('1b2ccf43d007f87e489765451bc47dc40db50c7780e98ce20cb8f7694ed8cbaf', 'hex');
-  //partyAlice.btc.publicKey = partyAlice.btc.btc_lowlevel_swap.getKeyPair(partyAlice.btc.privateKey).publicKey;
-  //partyBob.btc.publicKey = partyBob.btc.btc_lowlevel_swap.getKeyPair(partyBob.btc.privateKey).publicKey;
 
   const amount_btc = 1000;
   const amount_zano = 1000000000000;
@@ -50,16 +47,7 @@ async function perform_swap()
 
   //Generate a secrete for Aclice 
   const alice_secret = partyAlice.btc.getSecret();
-  //const alice_secret = partyAlice.btc.getSecret('c4c830491b74a28cb3e287b96e348846ce677e483e986f8340f32a872c29e18c');
 
-  //console.log('Alice secret key: ' + partyAlice.btc.privateKey.toString('hex'));
-  //console.log('Alice public key: ' + partyAlice.btc.publicKey.toString('hex'));
-  //console.log('Bob secret key: ' + partyBob.btc.privateKey.toString('hex'));
-  //console.log('Bob public key: ' + partyBob.btc.publicKey.toString('hex'));
-  
-  //const tmp = await partyAlice.btc.get_public_key();
-  
-  //console.log("Secrete: " + alice_secret.secret.toString('hex'));
   console.log("Secrete.hash: " + alice_secret.hash.toString('hex'));
 
   //Preparation step:
@@ -93,10 +81,7 @@ async function perform_swap()
   console.log("[BOB]: CREATING HTLC IN ZANO NTEWORK FOR ALICE.....");
   const bob_send_htlc_res = await partyBob.zano.send_htlc(await partyAlice.zano.get_address(), amount_zano, swap_time_zano, alice_secret.hash);
   console.log("[BOB]: CREATED, txid: " + JSON.stringify(bob_send_htlc_res.result.result_tx_id));
-  //comment this: 
-  //const bob_send_htlc_res = {txid: 'a78178e6933fcf58f784f39bd9866933aaa2550c31ccaacb2cac513b61aa1ae4'}; 
   
-
   console.log("[ALICE]: CHECKING ZANO HTLC CONFIRMED....");
   let alice_check_res = undefined;
   sleep_count = 0;
@@ -142,10 +127,6 @@ async function perform_swap()
     await sleep(1000);
   }
   console.log("[BOB]: CHECK IS ZANO HTLC REDEEMED. txid: " + bob_check_redeemed_res.result.redeem_tx_id);
-
-  //TODO: COMMENT IT
-  //const bob_check_redeemed_res = {result:{origin_secrete_as_hex: 'c4c830491b74a28cb3e287b96e348846ce677e483e986f8340f32a872c29e17c'}};
-
 
   console.log("[BOB]: REDEEMING BTC HTLC.....");
   const bob_redeem_res = await partyBob.btc.redeem_htlc(check_res.fundingTx, bob_check_redeemed_res.result.origin_secrete_as_hex, check_res.fundingOutput, swap_time_btc, partyAlice.btc.get_public_key());
